2744e7f9cf7c475e8700379590e09740e1761a80
Hrithik Hadawale <hrithikhadawale75@gmail.com>
2023-10-17 14:18:33 +0530

    ff

--- nauticalWeb/settings.py
+++ nauticalWeb/settings.py
@@ -132,14 +132,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'static/')
+STATIC_ROOT = os.path.join(BASE_DIR,'static/assets')
 # Media files
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 #static files
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "collected_static"),  # Assuming your static files are in a 'static' folder within your project
+    os.path.join(BASE_DIR, "static"),  # Assuming your static files are in a 'static' folder within your project
 ]# Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
